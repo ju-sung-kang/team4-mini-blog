c5a729d72c7f8aba33850f07e117f36f47617c60
ju-sung-kang <77263282+ju-sung-kang@users.noreply.github.com>
2021-05-28 00:43:09 +0900

    feat : posting list page layout (#1)

---
+++ src/components/header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+    width: 100%;
+    height: 30%;
+    color: white;
+    font-size: 2rem;
+    font-weight: 900%;
+    background-color: #2DB400;
+    text-align: center;
+`;
