6dd33467e8e198902e14f1ccf6be2a089e2697da
toodury <83403762+toodury@users.noreply.github.com>
2021-06-06 11:23:37 +0900

    feat : reply layout (#3)
    * feat : reply layout
    
    * docs : 더미 데이터 추가
    
    * docs : App.js 파일 수정
    
    충돌이 나지 않도록 App.js 파일 수정했습니다
    
    * feat : 파이어베이스 연동 완료, 답글 기능은 아직
    
    * feat : 파이어베이스 연동 완료 답글 기능 아직
    
    * docs : 필요없는 문서 삭제
    
    * feat : 유저 정보 연동 완료'
    
    * feat : 바뀐 데이터 구조 반영

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import WritePosting from './page/write-posting/index';
 import Main from './page/main/index';
+import Reply from './components/reply/index';
 
 function App() {
   return (
+    <>
     <Main/>
+    </>
   );
 }
 
---
+++ src/Firebase.js
@@ -0,0 +1,33 @@
+/*import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvCYXhlUrab62EH1AAjGcujV-2mivT-WE",
+    authDomain: "tinder-clone-7fd3b.firebaseapp.com",
+    projectId: "tinder-clone-7fd3b",
+    storageBucket: "tinder-clone-7fd3b.appspot.com",
+    messagingSenderId: "842829023616",
+    appId: "1:842829023616:web:8aeb30059a483b463f9965"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;*/
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBt9SNqM9ogAQZxpi8plzIrMXIA7WePhmI",
+    authDomain: "miniblog-92676.firebaseapp.com",
+    projectId: "miniblog-92676",
+    storageBucket: "miniblog-92676.appspot.com",
+    messagingSenderId: "42160782796",
+    appId: "1:42160782796:web:8a6c1dfe0439d555eeb169",
+    measurementId: "G-YVZ2301LFZ"
+  };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;
