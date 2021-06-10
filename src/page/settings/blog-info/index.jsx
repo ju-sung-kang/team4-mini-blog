7205d97650475e653dbed9d921e379b2cfa652cc
js-min <52990321+js-min@users.noreply.github.com>
2021-06-10 20:19:53 +0900

    Settings (#13)
    * feat : setting blog info
    
    * feat : setting blog info
    
    * feat : 메인 화면에 보여질 카테고리 설정 추가
    
    Co-authored-by: JISU MIN <jisumin@JISUs-MacBook-Pro.local>

--- src/page/settings/blog-info/index.jsx
+++ src/page/settings/blog-info/index.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 import * as S from './styles';
+import db from '../../../firebase';
 
 function SettingsBlogInfo() {
 
-    const [blogName, setBlogName] = useState("");
-    const [nickname, setNickname] = useState("");
-    const [about, setAbout] = useState("");
-    const [topic, setTopic] = useState("");
+    //const formRef = useRef();
+    const [blogInfo, setBlogInfo] = useState({blogName : "", nickname: "", profileUrl : "", mainCategory: ""})
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        return () => {
-        }
+        db.collection('blogInfo').doc('info').get()
+        .then((doc) => {
+            console.log(doc);
+            doc.exists && setBlogInfo(doc.data());
+        }).then(() => {
+            db.collection('categories')
+            .onSnapshot(snapshot => (
+            setCategories(snapshot.docs.map(doc => {
+                var data = doc.data();
+                data.id = doc.id;
+                return data;
+            }))))
+        })
+
     }, [])
 
     const saveInfo = (e) => {
         e.preventDefault();
-        console.log(nickname);
-        console.log(topic);
+        console.log(blogInfo);
+        // const current = formRef.current
+        db.collection('blogInfo').doc('info').update(blogInfo).then(() => {
+            alert("수정이 성공적으로 완료되었습니다.");
+        }).catch((error) => {
+            alert("수정 중에 오류가 발생하였습니다. 다시 시도해주세요.");
+        });
     }
 
     return (
@@ -33,14 +50,21 @@ function SettingsBlogInfo() {
                     <S.BlogInfoRow>
                         <S.BlogInfoRowHeader>블로그명</S.BlogInfoRowHeader>
                         <S.BlogInfoRowData>
-                            <S.BlogInfoInput type="text" onChange={e => setBlogName(e.target.value)} />
+                            <S.BlogInfoInput 
+                                type="text" 
+                                name="name" 
+                                value={blogInfo.name} 
+                                onChange={e => setBlogInfo({...blogInfo, name : e.target.value})} />
                             <S.InputDesc>한글, 영문, 숫자 혼용가능(한글 기준 25자 이내)</S.InputDesc>
                         </S.BlogInfoRowData>
                     </S.BlogInfoRow>
                     <S.BlogInfoRow>
                         <S.BlogInfoRowHeader>별명</S.BlogInfoRowHeader>
                         <S.BlogInfoRowData>
-                            <S.BlogInfoInput onChange={e => setNickname(e.target.value)} />
+                            <S.BlogInfoInput
+                                name="nickname"
+                                value={blogInfo.nickname}
+                                onChange={e => setBlogInfo({...blogInfo, nickname : e.target.value})} />
                             <S.InputDesc>한글, 영문, 숫자 혼용가능(한글 기준 10자 이내)</S.InputDesc>
                         </S.BlogInfoRowData>
                     </S.BlogInfoRow>
@@ -56,28 +80,26 @@ function SettingsBlogInfo() {
                         </S.BlogInfoRowData>
                     </S.BlogInfoRow>
                     <S.BlogInfoRow>
-                        <S.BlogInfoRowHeader>내 블로그 주제</S.BlogInfoRowHeader>
+                        <S.BlogInfoRowHeader>메인 카테고리</S.BlogInfoRowHeader>
                         <S.BlogInfoRowData>
-                            <S.BlogTopicSelect onChange={e => setTopic(e.target.value)}>
-                                <option value="">주제를 선택해주세요.</option>
-                                <option value="travel">여행</option>
-                                <option value="pet">반려동물</option>
-                                <option value="game">게임</option>
-                                <option value="movie">영화</option>
-                            </S.BlogTopicSelect>
+                            <S.BlogMainCategorySelect 
+                            name="mainCategory" 
+                            value={blogInfo.mainCategory} 
+                            onChange={e => setBlogInfo({...blogInfo, mainCategory : e.target.value})}>
+                                <option value="">기본으로 노출될 카테고리를 선택해주세요.</option>
+                                {categories.map((category) => <option value={category.id}>{category.name}</option>)}
+                            </S.BlogMainCategorySelect>
                             <S.InputDesc>
-                                내 블로그에서 다루는 주제를 선택하세요.
+                                기본으로 노출될 카테고리를 선택하세요.
                                 <br/>
-                                프로필 영역에 노출됩니다.
+                                메인 영역에 노출됩니다.
                             </S.InputDesc>
                         </S.BlogInfoRowData>
                     </S.BlogInfoRow>
                     <S.BlogInfoRow>
                         <S.BlogInfoRowHeader>블로그 프로필 이미지</S.BlogInfoRowHeader>
                         <S.BlogInfoRowData>
-                            <S.ProfileImageUploader>
-
-                            </S.ProfileImageUploader>
+                        <S.BlogInfoInput name="profileUrl" value={blogInfo.profileUrl} onChange={e => setBlogInfo({...blogInfo, profileUrl : e.target.value})} />
                         </S.BlogInfoRowData>
                     </S.BlogInfoRow>
                 </S.BlogInfoTableBody>
