3fe1c482d36a5fab21ea70966b8e5c7b46600b81
toodury <83403762+toodury@users.noreply.github.com>
2021-06-10 20:26:22 +0900

    파이어베이스 키 변동
    파이어베이스 키 변동

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,22 +1,3 @@
-<<<<<<< reply
-=======
-/*import firebase from 'firebase';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAvCYXhlUrab62EH1AAjGcujV-2mivT-WE",
-    authDomain: "tinder-clone-7fd3b.firebaseapp.com",
-    projectId: "tinder-clone-7fd3b",
-    storageBucket: "tinder-clone-7fd3b.appspot.com",
-    messagingSenderId: "842829023616",
-    appId: "1:842829023616:web:8aeb30059a483b463f9965"
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-
-export default db;*/
-
->>>>>>> main
 import firebase from 'firebase';
 
 const firebaseConfig = {
@@ -33,8 +14,4 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
-<<<<<<< reply
 export default db;
-=======
-export default db;
->>>>>>> main
