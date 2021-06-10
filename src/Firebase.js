bdf287f76100edf49a46645f9d8e6af2b0888251
toodury <83403762+toodury@users.noreply.github.com>
2021-06-10 19:53:17 +0900

    파이어베이스 지수님 키로 바꿨습니다
    파이어베이스 키 변동

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,4 @@
-/*import firebase from 'firebase';
+import firebase from 'firebase';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAvCYXhlUrab62EH1AAjGcujV-2mivT-WE",
@@ -8,24 +8,6 @@ const firebaseConfig = {
     messagingSenderId: "842829023616",
     appId: "1:842829023616:web:8aeb30059a483b463f9965"
 };
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-
-export default db;*/
-
-import firebase from 'firebase';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBt9SNqM9ogAQZxpi8plzIrMXIA7WePhmI",
-    authDomain: "miniblog-92676.firebaseapp.com",
-    projectId: "miniblog-92676",
-    storageBucket: "miniblog-92676.appspot.com",
-    messagingSenderId: "42160782796",
-    appId: "1:42160782796:web:8a6c1dfe0439d555eeb169",
-    measurementId: "G-YVZ2301LFZ"
-  };
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
