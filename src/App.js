39f7af372956595603b11220fb2888a85851005e
toodury <83403762+toodury@users.noreply.github.com>
2021-06-10 20:22:16 +0900

    docs: App.js 메인 페이지 수정
    main 에 있는 App.js 코드로 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
-import WritePosting from './page/write-posting/index';
 import Main from './page/main/index';
-import Reply from './components/reply/index';
+import SettingsCategory from './page/settings/category/index';
+import ViewPosting from './page/view-posting/index';
+import SettingsBlogInfo from './page/settings/blog-info/index'
 
 function App() {
   return (
-    <>
-    <Main/>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/settings/info">    
+          <SettingsBlogInfo />
+        </Route>
+        <Route exact path="/settings/category">    
+          <SettingsCategory />
+        </Route>
+        {/* <Route path="/post">
+          <ViewPosting />
+        </Route>  */}
+        <Route path="/">    {/* default root는 마지막에 작성 */}
+          <Main />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
