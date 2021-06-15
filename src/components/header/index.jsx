db94692a78d53fd907cea878c09f073507d5fb12
ju-sung-kang <jusung.dev@gmail.com>
2021-06-15 18:47:02 +0900

    feat : banner design added

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import db from '../../firebase.js';
 import * as S from './styles';
 
 const Header = () => {
+    const [bannerImage, setBannerImage] = useState();
+    useEffect(() => {
+        getBannerImage();
+    }, [])
+
+    const getBannerImage = () => {
+        db.collection('blogInfo').doc('info')
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                const tmp = doc.data();
+                setBannerImage(tmp.bannerImageUrl);
+            }
+            else {
+                console.log("Error getting banner image!");
+            }
+        }).catch((error) => {
+            console.log("Error getting banner image:", error);
+        });
+    }
+
     return (
-        <S.HeaderContainer>header</S.HeaderContainer>
+        <S.HeaderContainer>
+            <S.BannerImage src={bannerImage}/>
+            <S.BannerText>디카프리오의 블로그</S.BannerText>
+        </S.HeaderContainer>
     );
 }
 
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -3,9 +3,22 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.div`
     width: 100%;
     height: 15rem;
+    position: relative;
+`;
+
+export const BannerImage = styled.img`
+    width: 100%;
+    height: 15rem;
+    object-fit: cover;
+`;
+
+export const BannerText = styled.div`
+    border: 0.3rem solid white;
     color: white;
-    font-size: 2rem;
-    font-weight: 900%;
-    background-color: #2DB400;
-    text-align: center;
+    font-size: 3rem;
+    font-weight: 700;
+    position: absolute;
+    top: 50%;
+    left: 60%;
+    transform: translate( 0%, -50% );
 `;
