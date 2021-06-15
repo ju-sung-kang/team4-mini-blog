0b414768a0e73607e8e27c661c45f16eb134e285
ju-sung-kang <jusung.dev@gmail.com>
2021-06-15 13:43:32 +0900

    feat : setting으로 가는 버튼, 자잘한 디자인 몇가지

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect } from 'react';
 import * as S from './styles.js';
 import db from '../../firebase.js';
+import { useHistory } from 'react-router-dom';
 
 const Footer = (props) => {
 
     const API_KEY = "53da2272c20bab85b6e0a1ba478a531e";
+    const history = useHistory();
     const [loc, setLoc] = useState({lat: 0, long: 0});
     const [weather, setWeather] = useState({temperature: 0, name: "", icon: ""})
     const [today, setToday] = useState(0);
@@ -41,13 +43,13 @@ const Footer = (props) => {
     }
 
     const getUser = () => {
-        db.collection('blogInfo').doc('PkW2DmPU6YAZCQPBNc65')
+        db.collection('blogInfo').doc('info')
         .get()
         .then((doc) => {
             if (doc.exists) {
                 const tmp = doc.data();
                 setUser({
-                    image: tmp.profileImage,
+                    image: tmp.profileImageUrl,
                     name: tmp.nickName,
                     intro: tmp.introduction
                 });
@@ -85,8 +87,13 @@ const Footer = (props) => {
         });
     }
 
+    const goToSettingsBlogInfo = () => {
+        history.push("/settings/info")
+    }
 
-    //useEffect(getPosition, []);
+    const goToSettingsCategory = () => {
+        history.push("/settings/category")
+    }
 
     const img_url = `http://openweathermap.org/img/w/${weather.icon}.png`;
 
@@ -96,15 +103,20 @@ const Footer = (props) => {
                 <S.ProfileImage src={user.image}/>
                 <S.ProfileName>{user.name}</S.ProfileName>
                 <S.ProfileIntro>{user.intro}</S.ProfileIntro>
+                <S.Settings>
+                    <S.BlogInfoSettings onClick={goToSettingsBlogInfo}>😀 프로필 설정</S.BlogInfoSettings>
+                    <S.CategorySettings onClick={goToSettingsCategory}>🗂️ 카테고리 설정</S.CategorySettings>
+                </S.Settings>
             </S.Profile>
             <S.Category>
+                <S.CategoryLabel>카테고리</S.CategoryLabel>
                 {categoryList.length === 0 ? (
                     <S.NoCategory>설정에서 카테고리를 추가해주세요!</S.NoCategory>
                 ) : (
                   categoryList.map((category) => (
-                    <S.CategoryLabel key={category.categoryId} onClick={()=>categoryClick(category.categoryId)}>
+                    <S.CategoryTitle key={category.categoryId} onClick={()=>categoryClick(category.categoryId)}>
                         {category.categoryName}
-                    </S.CategoryLabel>
+                    </S.CategoryTitle>
                   ))
                 )}
             </S.Category>
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -3,30 +3,73 @@ import styled from 'styled-components';
 export const FooterContainer = styled.div`
     display: flex;
     width: 100%;
-    border: 1px solid gray;
+    margin-top: 3rem;
 `;
 
 //profile
 export const Profile = styled.div`
     display: flex;
     align-items: center;
+    justify-content: center;
     flex-wrap: wrap;
-    width: 30%;
-    border-right: 1px solid gray;
+    width: 20%;
+    border: 1px solid #D5D5D5;
+    background-color: #FDFDFB;
+    border-radius: 5%;
+    margin-right: 1rem;
 `;
 
 export const ProfileImage = styled.img`
-    width: 150px;
-    height: 150px;
+    width: 10rem;
+    height: 10rem;
     object-fit: cover;
 `;
 
 export const ProfileName = styled.p`
     width: 100%;
+    font-size: 1rem;
 `;
 
 export const ProfileIntro = styled.p`
     width: 100%;
+    font-size: 1rem;
+`;
+
+export const Settings = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+`;
+
+export const BlogInfoSettings = styled.div`
+    width: 80%;
+    height: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid gray;
+    background-color: #FFFFFF;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    cursor: pointer;
+    font-size: 1.1rem;
+    font-weight: 700;
+`;
+
+export const CategorySettings = styled.div`
+    width: 80%;
+    height: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid gray;
+    background-color: #FFFFFF;
+    margin-bottom: 1rem;
+    cursor: pointer;
+    font-size: 1.1rem;
+    font-weight: 700;
 `;
 
 // category
@@ -36,11 +79,15 @@ export const Category = styled.div`
     border-right: 1px solid gray;
 `;
 
+export const CategoryLabel = styled.div`
+    color: red;
+`;
+
 export const NoCategory = styled.div`
     width: 100%;
 `;
 
-export const CategoryLabel = styled.div`
+export const CategoryTitle = styled.div`
     width: 100%;
     height: 4rem;
     &: hover {
@@ -51,4 +98,3 @@ export const CategoryLabel = styled.div`
 export const Weather = styled.div`
     width: 30%;
 `;
-
