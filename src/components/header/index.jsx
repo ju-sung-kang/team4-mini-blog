bfa1ab01b993881e111a1b230e440329e95a2045
ju-sung-kang <77263282+ju-sung-kang@users.noreply.github.com>
2021-06-18 23:03:34 +0900

    Write posting mark down (#44)
    * feat : setting으로 가는 버튼, 자잘한 디자인 몇가지
    
    * feat : profile design
    
    * feat : banner design added
    
    * feat : 메인페이지 css 디자인 입히기
    
    * feat : mark down editor 구현, 글쓰기 페이지 디자인 완료
    
    * feat : main design and write posting design and view mark down
    
    * feat : 라우팅 페이지 명칭 변경
    
    * feat : banner 누르면 다시 홈으로 라우팅
    
    * Update App.js
    
    * Update index.jsx
    
    * Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import db from '../../firebase.js';
 import * as S from './styles';
 
 const Header = () => {
+    const history = useHistory();
     const [bannerImage, setBannerImage] = useState();
     const [blogName, setBlogName] = useState();
     const [userName, setUserName] = useState();
@@ -29,10 +31,14 @@ const Header = () => {
         });
     }
 
+    const goToMain = () => {
+        history.push('/');
+    }
+
     return (
         <S.HeaderContainer>
             <S.BannerText>{blogName} &nbsp; <S.nameText>ㅡ</S.nameText> &nbsp;by {userName}.</S.BannerText>
-            <S.BannerImage src={bannerImage}/>
+            <S.BannerImage src={bannerImage} onClick={goToMain}/>
         </S.HeaderContainer>
     );
 }
